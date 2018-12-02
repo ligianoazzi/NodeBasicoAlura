0b51466fb68aa945771b39434f5ebf187f84247b
ligianoazzi <ldaazzi@gmail.com>
2018-12-02 21:24:42 +0000

    merge my code with code of the instructor

--- server.js
+++ server.js
@@ -1,36 +1,5 @@
-const http =  require('http');
+const app = require('./src/config/custom-express');
 
-
-
-const servidor = http.createServer(function (req, resp){
-
-
-var html = "";
-
-if(req.url == '/'){
-	html = "caiu no /";
-}else if(req.url == '/livros'){
-	html = "caiu no /livros";
-}
-
-
-
-
-	resp.end(html);
-}); // isto já é do node
-
-servidor.listen(3000);
-
-
-
-
-
-
-/*
-passo a passo:
-1 - acessar cmd, pasta onde está o arquivo server.js
-2 - rodar: node server.js
-3 - no browser acessar localhost:3000 (ou o numero da porta na linha 7)
-
-
-*/
+app.listen(3000, function() {
+    console.log(`Servidor rodando na porta 3000`);
+});
